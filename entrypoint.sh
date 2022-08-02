ca7dd0d1677b91d20e17ccefb02c7a5327399b8b
Preetul <47962979+Preetul@users.noreply.github.com>
2022-08-02 09:49:29 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $1 > message.txt
-curl $2 -X post -H 'Content-type: application/json'  --data "@message.txt"
+echo $2 > message.txt
+curl $1 -X post -H 'Content-type: application/json'  --data "@message.txt"
